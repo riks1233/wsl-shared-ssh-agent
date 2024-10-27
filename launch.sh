0eb5513cea6b249c59d353d3b3c62cdfef4fecfd
Richardas Keršis <richardas.kersis@gmail.com>
2024-10-27 07:18:30 +0200

    Restrict env file permissions + comment improvements

--- launch.sh
+++ launch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 script_dir="$(dirname "$BASH_SOURCE")"
+# "env file" to store running SSH Agent's environment variables.
 env_file="$script_dir/.env"
 
 print() {
@@ -12,7 +13,7 @@ print() {
 if pgrep -u "$USER" ssh-agent >/dev/null; then
   print "ssh-agent is already running."
   if [[ -f $env_file ]]; then
-    # Just evaluate the stored environment variables that point to SSH Agent running process.
+    # Evaluate the stored environment variables from env file.
     eval "$(cat $env_file)"
   else
     print "Unable to locate $env_file file, this shouldn't happen."
@@ -20,8 +21,10 @@ if pgrep -u "$USER" ssh-agent >/dev/null; then
 
 else
   print "Starting ssh-agent."
-  # Start the SSH Agent and store its environment variables in a file.
+  # Start the SSH Agent and store its environment variables in env file.
   ssh-agent -s >$env_file
+  # Restrict env file permissions.
+  chmod 600 $env_file
   # Evaluate the stored environment variables.
   eval "$(cat $env_file)"
   # Run an ssh-add command for convenience. Can always be interrupted.
