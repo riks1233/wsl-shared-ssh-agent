6688ae7ceaccd5d79b337a5e6eec20f8f754e687
Richardas Keršis <richardas.kersis@gmail.com>
2024-11-12 08:38:47 +0200

    Added listing of added identities in case ssh-agent is already running

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 script_dir="$(dirname "$BASH_SOURCE")"
-# "env file" to store running SSH Agent's environment variables.
+# ".env file" to store running SSH Agent's environment variables.
 env_file="$script_dir/.env"
 
 print() {
@@ -13,17 +13,19 @@ print() {
 if pgrep -u "$USER" ssh-agent >/dev/null; then
   print "ssh-agent is already running."
   if [[ -f $env_file ]]; then
-    # Evaluate the stored environment variables from env file.
+    # Evaluate the stored environment variables from .env file.
     eval "$(cat $env_file)"
+    # List the added identities.
+    ssh-add -l
   else
     print "Unable to locate $env_file file, this shouldn't happen."
   fi
 
 else
   print "Starting ssh-agent."
-  # Start the SSH Agent and store its environment variables in env file.
+  # Start the SSH Agent and store its environment variables in .env file.
   ssh-agent -s >$env_file
-  # Restrict env file permissions.
+  # Restrict .env file permissions.
   chmod 600 $env_file
   # Evaluate the stored environment variables.
   eval "$(cat $env_file)"
